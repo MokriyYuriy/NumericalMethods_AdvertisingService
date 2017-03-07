ca08cd7c040f1a84eee2ca3c3c2a32ec959d1d2f
YuryMokriy <mavrandr@mail.ru>
2017-03-07 11:47:24 +0300

    Final version 3

--- adv_server/tabulate.py
+++ adv_server/tabulate.py
@@ -3,7 +3,7 @@ import numpy as np
 class TabulatedFunction(object):
     def __init__(self, arguments, values):
         if len(arguments) != len(values):
-            ValueError("Arguments and values have different length")
+            raise ValueError("Arguments and values have different length")
         self.arguments = np.array(arguments)
         self.values = np.array(values)
 
@@ -12,12 +12,12 @@ class TabulatedFunction(object):
 
 def make_uniform_grid(left, right, num):
     if (num < 2):
-        ValueError('The argument npoints has to be at least 2')
+        raise ValueError('The argument npoints has to be at least 2')
     return np.linspace(start=left, stop=right, num=num, endpoint=True)
 
 def make_chebyshev_grid(left, right, num):
     if (num < 2):
-        ValueError('The argument npoints has to be at least 2')
+        raise ValueError('The argument npoints has to be at least 2')
     return (right + left) / 2 + (right - left) / 2 * np.cos((np.array(list(range(1, 2 * num + 2, 2))) - 1) / 2 / num * np.pi)
 
 def make_uniform_tabulation(f, left, right, num):
